e112861ba65668be1ffb482fe5b277528dea97fc
tanjoin <tanjoin@users.noreply.github.com>
2017-03-22 13:30:37 +0900

    改良

--- README.md
+++ README.md
@@ -13,3 +13,10 @@ npm install
 ```
 node index.js
 ```
+
+## Resources
+
+- [野球のボールのイラスト | かわいいフリー素材集 いらすとや](http://www.irasutoya.com/2016/04/blog-post_661.html)
+- [野球のフィールドのイラスト | かわいいフリー素材集 いらすとや](http://www.irasutoya.com/2013/09/blog-post_3476.html)
+- [ウグイス嬢のイラスト（スポーツ） | かわいいフリー素材集 いらすとや](http://www.irasutoya.com/2017/01/blog-post_458.html)
+- [実況アナウンサーのイラスト | かわいいフリー素材集 いらすとや](http://www.irasutoya.com/2014/01/blog-post_4710.html)
--- index.js
+++ index.js
@@ -1,30 +1,42 @@
 var Nightmare = require('nightmare');
-var nightmare = Nightmare({ show : false });
+var nightmare = Nightmare({
+    show: false
+});
 var notifier = require('node-notifier');
 
 var text = '';
 
 var main = function() {
-  nightmare
-    .goto('https://baseball.yahoo.co.jp/live/wbc/game/2017032211/score')
-    .wait('#situation')
-    .evaluate(function() {
-      return document.querySelector('#situation > p').innerText;
-    })
-    .end()
-    .then(function(result) {
-      if (text !== result) {
-        notifier.notify({
-          'title': 'WBC速報',
-          'message': result
+    nightmare
+        .goto('https://baseball.yahoo.co.jp/live/wbc/game/2017032211/score')
+        .wait('#situation')
+        .evaluate(function() {
+            return document.querySelector('#situation > p').innerText;
+        })
+        .end()
+        .then(function(result) {
+            if (text !== result) {
+                notifier.notify({
+                    'title': 'WBC速報',
+                    'message': result,
+                    'icon': 'res/baseball_ball.png',
+                    'open': 'https://baseball.yahoo.co.jp/live/wbc/game/2017032211/score'
+                });
+                text = result;
+            }
+        })
+        .catch(function(error) {
+            console.error(error);
+            notifier.notify({
+                'title': 'WBC速報',
+                'message': 'エラーが発生したため通知を終了します',
+                'icon': 'res/job_uguisujou_sports.png',
+                'open': 'https://baseball.yahoo.co.jp/live/wbc/game/2017032211/score'
+            });
+            clearInterval(loop);
         });
-        text = result;
-      }
-    })
-    .catch(function(error) {
-      console.error(error);
-    });
 };
 
-main();
-setInterval(main, 60000); // 1分おき
+var loop = setInterval(function() {
+    main();
+}, 10000); // 10秒おき
